2cae91e74e473d4a04bf0ea784b013b032965ffd
coseguera <coseguera@users.noreply.github.com>
2015-04-15 00:26:18 +0000

    transaction api routes with specs

--- routes/api/transactions.js
+++ routes/api/transactions.js
@@ -1,24 +1,114 @@
 'use strict';
 
 module.exports = function (repo, logger) {
+    function isValidDate(d) {
+        return !isNaN(d.getTime());
+    }
+
     return {
         '/': {
-        },
-        '/:id': {
-            put: function (req, res) {
-                var id = req.params.id;
+            // temporary get function to start with
+            get: function (req, res) {
+                repo.getAll(function (err, result) {
+                    if (err) {
+                        logger.error(err);
+                        res.sendStatus(500);
+                        return;
+                    }
 
+                    res.json(result);
+                });
+            },
+            post: function (req, res) {
                 var obj = {
-                    date: Date.parse(req.body.date),
+                    date: new Date(req.body.date),
                     concept: req.body.concept,
                     amount: req.body.amount,
                     person: req.body.person,
                     account: req.body.account
                 };
 
+                if (!req.body.date ||
+                    !isValidDate(obj.date) ||
+                    !obj.concept || 
+                    !obj.amount || 
+                    isNaN(obj.amount) ||
+                    !obj.person || 
+                    !obj.account) {
+                    res.sendStatus(400);
+                    return;
+                }
+
+                repo.create(obj, function (err) {
+                    if (err) {
+                        logger.error(err);
+                        res.sendStatus(500);
+                        return;
+                    }
+
+                    res.sendStatus(200);
+                });
+            }
+        },
+        '/:id': {
+            get: function (req, res) {
+                var id = req.params.id;
+
+                repo.get(id, function (err, result){
+                    if (err) {
+                        logger.error(err);
+                        res.sendStatus(500);
+                        return;
+                    }
+
+                    if (!result) {
+                        res.sendStatus(404);
+                        return;
+                    }
+
+                    res.json(result);
+                });
+            },
+            put: function (req, res) {
+                var id = req.params.id;
+
+                var obj = {
+                    date: new Date(req.body.date),
+                    concept: req.body.concept,
+                    amount: req.body.amount,
+                    person: req.body.person,
+                    account: req.body.account
+                };
+
+                if (!req.body.date ||
+                    !isValidDate(obj.date) ||
+                    !obj.concept ||
+                    !obj.amount ||
+                    isNaN(obj.amount) ||
+                    !obj.person ||
+                    !obj.account) {
+                    res.sendStatus(400);
+                    return;
+                }
+
                 if(req.body.splitId) {
                     obj.splitId = req.body.splitId;
                 }
+
+                repo.update(id, obj, function (err, result) {
+                    if (err) {
+                        logger.error(err);
+                        res.sendStatus(500);
+                        return;
+                    }
+
+                    if (!result) {
+                        res.sendStatus(404);
+                        return;
+                    }
+
+                    res.sendStatus(200);
+                });
             },
             delete: function (req, res) {
                 var id = req.params.id;
--- spec/routes/api/transactions-spec.js
+++ spec/routes/api/transactions-spec.js
@@ -14,7 +14,345 @@ describe('transaction routes', function () {
         spyOn(resMock, 'json');
     });
 
-    describe(':id METHOD DELETE', function () {
+    describe('index METHOD: POST', function () {
+        it('creates a transaction', function () {
+            var repoMock = repositoryMock();
+            repoMock.err = 0;
+
+            var reqMock = {
+                body: {
+                    date: '04/14/2015',
+                    concept: 'concept',
+                    amount: 100,
+                    person: 'person',
+                    account: 'account',
+                    splitId: 'splitId'
+                }
+            };
+
+            var obj = routes(repoMock, loggerMock);
+
+            obj['/'].post(reqMock, resMock);
+
+            expect(resMock.sendStatus).toHaveBeenCalledWith(200);
+        });
+
+        it('returns 500 if there is an error', function () {
+            var repoMock = repositoryMock();
+            repoMock.err = 1;
+
+            var reqMock = {
+                body: {
+                    date: '04/14/2015',
+                    concept: 'concept',
+                    amount: 100,
+                    person: 'person',
+                    account: 'account',
+                    splitId: 'splitId'
+                }
+            };
+
+            var obj = routes(repoMock, loggerMock);
+
+            obj['/'].post(reqMock, resMock);
+
+            expect(resMock.sendStatus).toHaveBeenCalledWith(500);
+        });
+
+        it('returns 400 if no date is provided', function () {
+            var repoMock = repositoryMock();
+            repoMock.err = 1;
+
+            var reqMock = {
+                body: { // note there is no date
+                    concept: 'concept',
+                    amount: 100,
+                    person: 'person',
+                    account: 'account',
+                    splitId: 'splitId'
+                }
+            };
+
+            var obj = routes(repoMock, loggerMock);
+
+            obj['/'].post(reqMock, resMock);
+
+            expect(resMock.sendStatus).toHaveBeenCalledWith(400);
+        });
+
+        it('returns 400 if an invalid date is provided', function () {
+            var repoMock = repositoryMock();
+            repoMock.err = 0;
+            repoMock.result = 1;
+
+            var reqMock = {
+                body: {
+                    date: 'date', // note date is not a valid date
+                    concept: 'concept',
+                    amount: 100,
+                    person: 'person',
+                    account: 'account',
+                    splitId: 'splitId'
+                }
+            };
+
+            var obj = routes(repoMock, loggerMock);
+
+            obj['/'].post(reqMock, resMock);
+
+            expect(resMock.sendStatus).toHaveBeenCalledWith(400);
+        });
+
+        it('returns 400 if an invalid amount is provided', function () {
+            var repoMock = repositoryMock();
+            repoMock.err = 0;
+            repoMock.result = 1;
+
+            var reqMock = {
+                body: {
+                    date: '04/14/2015',
+                    concept: 'concept',
+                    amount: 'amount', // note amount is not a valid number
+                    person: 'person',
+                    account: 'account',
+                    splitId: 'splitId'
+                }
+            };
+
+            var obj = routes(repoMock, loggerMock);
+
+            obj['/'].post(reqMock, resMock);
+
+            expect(resMock.sendStatus).toHaveBeenCalledWith(400);
+        });
+    });
+
+    describe(':id METHOD: GET', function () {
+        it('returns transaction from repo', function () {
+            var repoMock = repositoryMock();
+            repoMock.err = 0;
+            repoMock.result = 2;
+
+            var reqMock = {
+                params: { id: 'id' }
+            };
+
+            var obj = routes(repoMock, loggerMock);
+
+            obj['/:id'].get(reqMock, resMock);
+
+            expect(resMock.json).toHaveBeenCalledWith(repoMock.result);
+        });
+
+        it('returns 500 if there is an error', function () {
+            var repoMock = repositoryMock();
+            repoMock.err = 1;
+
+            var reqMock = {
+                params: { id: 'id' }
+            };
+
+            var obj = routes(repoMock, loggerMock);
+
+            obj['/:id'].get(reqMock, resMock);
+
+            expect(resMock.sendStatus).toHaveBeenCalledWith(500);
+            expect(resMock.json).not.toHaveBeenCalled();
+        });
+ 
+        it('returns 404 if id is not present', function () {
+            var repoMock = repositoryMock();
+            repoMock.err = 0;
+            repoMock.result = 0;
+
+            var reqMock = {
+                params: { id: 'id' }
+            };
+
+            var obj = routes(repoMock, loggerMock);
+
+            obj['/:id'].get(reqMock, resMock);
+
+            expect(resMock.sendStatus).toHaveBeenCalledWith(404);
+            expect(resMock.json).not.toHaveBeenCalled();
+        });
+    });
+
+    describe(':id METHOD: PUT', function () {
+        it('updates a transaction in repo', function () {
+            var repoMock = repositoryMock();
+
+            repoMock.update = function (id, obj, callback) {
+                callback(this.err, this.result);
+            };
+
+            repoMock.err = 0;
+            repoMock.result = 1;
+
+            var reqMock = {
+                params: { id: 'id' },
+                body: {
+                    date: '04/14/2015',
+                    concept: 'concept',
+                    amount: 100,
+                    person: 'person',
+                    account: 'account',
+                    splitId: 'splitId'
+                }
+            };
+
+            var obj = routes(repoMock, loggerMock);
+
+            obj['/:id'].put(reqMock, resMock);
+
+            expect(resMock.sendStatus).toHaveBeenCalledWith(200);
+        });
+
+        it('returns 500 if there is an error', function () {
+            var repoMock = repositoryMock();
+
+            repoMock.update = function (id, obj, callback) {
+                callback(this.err, this.result);
+            };
+
+            repoMock.err = 1;
+
+            var reqMock = {
+                params: { id: 'id' },
+                body: {
+                    date: '04/14/2015',
+                    concept: 'concept',
+                    amount: 100,
+                    person: 'person',
+                    account: 'account',
+                    splitId: 'splitId'
+                }
+            };
+
+            var obj = routes(repoMock, loggerMock);
+
+            obj['/:id'].put(reqMock, resMock);
+
+            expect(resMock.sendStatus).toHaveBeenCalledWith(500);
+        });
+
+        it('returns 404 if transaction doesnt exist', function () {
+            var repoMock = repositoryMock();
+
+            repoMock.update = function (id, obj, callback) {
+                callback(this.err, this.result);
+            };
+
+            repoMock.err = 0;
+            repoMock.result = 0;
+
+            var reqMock = {
+                params: { id: 'id' },
+                body: {
+                    date: '04/14/2015',
+                    concept: 'concept',
+                    amount: 100,
+                    person: 'person',
+                    account: 'account',
+                    splitId: 'splitId'
+                }
+            };
+
+            var obj = routes(repoMock, loggerMock);
+
+            obj['/:id'].put(reqMock, resMock);
+
+            expect(resMock.sendStatus).toHaveBeenCalledWith(404);
+        });
+
+        it('returns 400 if no date is provided', function () {
+            var repoMock = repositoryMock();
+
+            repoMock.update = function (id, obj, callback) {
+                callback(this.err, this.result);
+            };
+
+            repoMock.err = 0;
+            repoMock.result = 1;
+
+            var reqMock = {
+                params: { id: 'id' },
+                body: { // note there is no date
+                    concept: 'concept',
+                    amount: 100,
+                    person: 'person',
+                    account: 'account',
+                    splitId: 'splitId'
+                }
+            };
+
+            var obj = routes(repoMock, loggerMock);
+
+            obj['/:id'].put(reqMock, resMock);
+
+            expect(resMock.sendStatus).toHaveBeenCalledWith(400);
+        });
+
+        it('returns 400 if an invalid date is provided', function () {
+            var repoMock = repositoryMock();
+
+            repoMock.update = function (id, obj, callback) {
+                callback(this.err, this.result);
+            };
+
+            repoMock.err = 0;
+            repoMock.result = 1;
+
+            var reqMock = {
+                params: { id: 'id' },
+                body: {
+                    date: 'date', // note date is not a valid date
+                    concept: 'concept',
+                    amount: 100,
+                    person: 'person',
+                    account: 'account',
+                    splitId: 'splitId'
+                }
+            };
+
+            var obj = routes(repoMock, loggerMock);
+
+            obj['/:id'].put(reqMock, resMock);
+
+            expect(resMock.sendStatus).toHaveBeenCalledWith(400);
+        });
+
+        it('returns 400 if an invalid amount is provided', function () {
+            var repoMock = repositoryMock();
+
+            repoMock.update = function (id, obj, callback) {
+                callback(this.err, this.result);
+            };
+
+            repoMock.err = 0;
+            repoMock.result = 1;
+
+            var reqMock = {
+                params: { id: 'id' },
+                body: {
+                    date: '04/14/2015',
+                    concept: 'concept',
+                    amount: 'amount', // note amount is not a valid number
+                    person: 'person',
+                    account: 'account',
+                    splitId: 'splitId'
+                }
+            };
+
+            var obj = routes(repoMock, loggerMock);
+
+            obj['/:id'].put(reqMock, resMock);
+
+            expect(resMock.sendStatus).toHaveBeenCalledWith(400);
+        });
+    });
+
+    describe(':id METHOD: DELETE', function () {
         it('deletes a transaction in repo', function () {
             var repoMock = repositoryMock();
             repoMock.err = 0;
