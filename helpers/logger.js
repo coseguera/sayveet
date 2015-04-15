a5b726bed5b949e59aa2a3f252b80a8e63a7e20b
coseguera <coseguera@users.noreply.github.com>
2015-04-15 00:17:18 -0700

    activated api routes

---
+++ helpers/logger.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports = {
+    error: function (err) {
+        console.error(err);
+    },
+    log: function (msg) {
+        console.log(msg);
+    }
+};
--- routes/api.js
+++ routes/api.js
@@ -1,43 +1,43 @@
-//'use strict';
-//
-//var express = require('express');
-//var router = express.Router();
-//var mongoose = require('mongoose');
-//
-//var accountRepository = require('./../models/db/accountRepository');
-//var accountModel = require('./../models/db/accountModel');
-//var accountRoutes = require('./api/accounts');
-//
-//var personRepository = require('./../models/db/personRepository');
-//var personModel = require('./../models/db/personModel');
-//var personRoutes = require('./api/people');
-//
-//var db = mongoose.createConnection('mongodb://localhost/sayveettestb');
-//
-//var logger = {
-//    error: function (err) {
-//        console.error(err);
-//    }
-//};
-//
-//accountModel();
-//var accountRepo = accountRepository(db.model('Account'));
-//var accounts = accountRoutes(accountRepo, logger);
-//
-//router.get('/account', accounts['/'].get);
-//router.post('/account', accounts['/'].post);
-//router.get('/account/:id', accounts['/:id'].get);
-//router.put('/account/:id', accounts['/:id'].put);
-//router.delete('/account/:id', accounts['/:id'].delete);
-//
-//personModel();
-//var personRepo = personRepository(db.model('Person'));
-//var people = personRoutes(personRepo, logger);
-//
-//router.get('/person', people['/'].get);
-//router.post('/person', people['/'].post);
-//router.get('/person/:id', people['/:id'].get);
-//router.put('/person/:id', people['/:id'].put);
-//router.delete('/person/:id', people['/:id'].delete);
-//
-//module.exports = router;
+'use strict';
+
+var express = require('express');
+var router = express.Router();
+var mongoose = require('mongoose');
+var logger = require('./../helpers/logger');
+
+var db = mongoose.createConnection('mongodb://localhost/sayveettestb');
+
+var entities = [ 
+    { name: 'account', model: 'Account' }, 
+    { name: 'person', model: 'Person' }, 
+    { name: 'transaction', model: 'Transaction' } 
+];
+
+for (var i in entities) {
+    if (entities.hasOwnProperty(i)) {
+        var entity = entities[i];
+        var repoFn = require('./../models/db/' + entity.name + 'Repository');
+        var modelFn = require('./../models/db/' + entity.name + 'Model');
+        var routeFn = require('./api/' + entity.name);
+    
+        modelFn();
+        var repo = repoFn(db.model(entity.model));
+        var routes = routeFn(repo, logger);
+    
+        for (var key in routes) {
+            if (routes.hasOwnProperty(key)) {
+                for (var method in routes[key]) {
+                    if (routes[key].hasOwnProperty(method)) {
+                        router[method]('/' + entity.name + key, 
+                                       routes[key][method]);
+
+                        logger.log('activated ' + entity.name + key +
+                                   ' ' + method + ' route');
+                    }
+                }
+            }
+        }
+    }
+}
+
+module.exports = router;
