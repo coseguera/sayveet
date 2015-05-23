b6a14d4535c8d50228e2bda462df77921a930b70
coseguera <coseguera@users.noreply.github.com>
2015-05-23 00:45:34 -0700

    gulp-jshint

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,12 @@
-var gulp = require('gulp');
+'use strict';
 
-gulp.task('default', function () {
+var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+
+gulp.task('default', ['lint']);
+
+gulp.task('lint', function () {
+    gulp.src('./**/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
 });
