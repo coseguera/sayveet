af9c1523ec646bccef52fe1eda053d7530dbe1c1
coseguera <coseguera@users.noreply.github.com>
2015-06-07 23:14:24 -0700

    making the scalable require approach simple to use

--- public/javascripts/sayveet.js
+++ public/javascripts/sayveet.js
@@ -1,3 +1,8 @@
 require(['/javascripts/require-config.js'], function () {
-	require(['jquery', 'bootstrap'], function () { });
+	require(['jquery', 'bootstrap'], function ($) {
+		var $script = $('#require');
+		if($script.data('load')) {
+			require([$script.data('load')]);
+		}
+	});
 });
